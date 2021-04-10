ec5b22f446f7bd28b7eb05cb2331f6e6915a6eef
Amit P Naik <amit.naik8103@gmail.com>
2021-04-10 23:35:44 +0530

    feat: initial folder structure

---
+++ src/BuildingBlocks/Microservice.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Microservice.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/BuildingBlocks/Microservice.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Microservice.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
