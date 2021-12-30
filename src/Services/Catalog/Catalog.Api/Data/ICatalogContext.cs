d0eaf80f5ee8bda40366ac60cf55e4d1ec13a1fc
Amit P Naik <amit.naik8103@gmail.com>
2021-12-30 15:58:08 +0530

    feat: catalog api

---
+++ src/Services/Catalog/Catalog.Api/Data/ICatalogContext.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Repositories/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
